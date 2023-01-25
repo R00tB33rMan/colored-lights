fd2563fd783d09d1c0db9fd246336e0024393696
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-01-25 12:18:04 +0100

    Fixed LevelChunkSectionMixin

--- src/main/java/dev/gegy/colored_lights/mixin/chunk/LevelChunkSectionMixin.java
+++ src/main/java/dev/gegy/colored_lights/mixin/chunk/LevelChunkSectionMixin.java
@@ -2,6 +2,7 @@ package dev.gegy.colored_lights.mixin.chunk;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -21,16 +22,17 @@ public abstract class LevelChunkSectionMixin implements ColoredLightChunkSection
     @Shadow
     public abstract boolean hasOnlyAir();
     
+    @Unique
     private ColoredLightValue[] coloredLightPoints;
+    @Unique
     private int coloredLightGeneration;
     
-    @Inject(method = "setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getFluidState()Lnet/minecraft/fluid/FluidState;", ordinal = 0),
+    @Inject(method = "setBlockState(IIILnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/world/level/block/state/BlockState;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getFluidState()Lnet/minecraft/world/level/material/FluidState;", ordinal = 0),
             locals = LocalCapture.CAPTURE_FAILHARD, require = 1)
     private void setBlockState(int x, int y, int z, BlockState state, boolean lock, CallbackInfoReturnable<BlockState> ci, BlockState lastState) {
-        if (lastState.getLightEmission() != 0 || state.getLightEmission() != 0) {
+        if (lastState.getLightEmission() != 0 || state.getLightEmission() != 0)
             this.invalidateColoredLight();
-        }
     }
     
     @Inject(method = "recalcBlockCounts", at = @At("HEAD"), require = 1)
@@ -49,9 +51,8 @@ public abstract class LevelChunkSectionMixin implements ColoredLightChunkSection
             return ColoredLightValue.NO;
         
         var points = this.coloredLightPoints;
-        if (points == null) {
+        if (points == null)
             this.coloredLightPoints = points = ChunkColoredLightSampler.sampleCorners(world, sectionPos, (LevelChunkSection) (Object) this);
-        }
         
         return points[ChunkColoredLightSampler.octantIndex(x, y, z)];
     }
